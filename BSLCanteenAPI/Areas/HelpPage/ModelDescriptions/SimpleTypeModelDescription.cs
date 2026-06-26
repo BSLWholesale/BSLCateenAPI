4d89de7f36dbe229a179bfee31dadfd914b08799
Kanchan <banswaraws@gmail.com>
2026-06-26 11:03:41 +0530

    Add project files.

---
+++ BSLCanteenAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BSLCanteenAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BSLCanteenAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BSLCanteenAPI.WebApiApplication" Language="C#" %>
