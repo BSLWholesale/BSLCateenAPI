797bb97946a82c7ef8ec197164c47623aa483645
Kanchan <banswaraws@gmail.com>
2026-06-30 10:07:12 +0530

    Added the new data access layer DALEmployee.cs - Kanchan

---
+++ BSLCanteenAPI/DAL/DALEmployee.cs
@@ -0,0 +1,280 @@
+﻿using BSLCanteenAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace BSLCanteenAPI.DAL
+{
+    public class DALEmployee
+    {
+        SqlConnection Con = new SqlConnection(ConfigurationManager.ConnectionStrings["BSL"].ConnectionString);
+
+
+        public clsEmployee Fn_Login_Employee(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (objReq.EmpId == null || objReq.EmpId == 0 && objReq.EmpMobile == null || objReq.EmpMobile == "0")
+                {
+                    objResp.vErrorMsg = "Please Enter Employee ID";
+                }
+                else if (String.IsNullOrWhiteSpace(objReq.EmpPassword))
+                {
+                    objResp.vErrorMsg = "Please Enter the Password";
+                }
+                else
+                {
+                    if (Con.State == ConnectionState.Broken)
+                    { Con.Close(); }
+                    if (Con.State == ConnectionState.Closed)
+                    { Con.Open(); }
+
+                    string encryptPassword = Generic.EncryptText(objReq.EmpPassword);
+
+                    SqlCommand cmd = new SqlCommand("USP_Employee", Con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpId", objReq.EmpId);
+                    cmd.Parameters.AddWithValue("@EmpMobile", objReq.EmpMobile);
+                    cmd.Parameters.AddWithValue("@EmpPassword", encryptPassword);
+                    cmd.Parameters.AddWithValue("@QueryType", "LogIn");
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    int i = 0;
+
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        string decryptPassword = Generic.DecryptText(Convert.ToString(ds.Tables[0].Rows[i]["EmpPassword"]));
+
+                        objResp.EmpId = Convert.ToInt64(ds.Tables[0].Rows[i]["EmployeeId"]);
+                        objResp.EmpName = Convert.ToString(ds.Tables[0].Rows[i]["Name"]);
+                        objResp.EmpMobile = Convert.ToString(ds.Tables[0].Rows[i]["EmpMobile"]);
+                        objResp.Department = Convert.ToString(ds.Tables[0].Rows[i]["Department"]);
+                        objResp.Location = Convert.ToString(ds.Tables[0].Rows[i]["Location"]);
+
+                        objResp.vErrorMsg = "Success";
+                    }
+                    else
+                    {
+                        objResp.vErrorMsg = "Entered Credentials has been invalid";
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Logger.WriteLog("Function Name : Fn_Login_Employee", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+        public clsEmployee Fn_Insert_Employee(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                SqlCommand cmd = new SqlCommand("USP_Employee", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", objReq.EmpId);
+                cmd.Parameters.AddWithValue("@Name", objReq.EmpName);
+                cmd.Parameters.AddWithValue("@Department", objReq.Department);
+                cmd.Parameters.AddWithValue("@Salary", objReq.Salary);
+                cmd.Parameters.AddWithValue("@EmpPassword", objReq.EmpPassword);
+                cmd.Parameters.AddWithValue("@EmpMobile", objReq.EmpMobile);
+                cmd.Parameters.AddWithValue("@Location", objReq.Location);
+                cmd.Parameters.AddWithValue("@QueryType", "InsertEmployee");
+
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    objResp.vErrorCode = 200;
+                    objResp.vErrorMsg = "Success";
+                }
+                else
+                {
+                    objResp.vErrorCode = 400;
+                    objResp.vErrorMsg = "Employee Insertion failed";
+                }
+            }
+            catch (Exception exp)
+            {
+                objResp.vErrorCode = 500;
+                Logger.WriteLog("Function Name : Fn_Insert_Employee", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+        public clsEmployee Fn_Fetch_EmployeeDetails(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (objReq.EmpId == 0)
+                {
+                    objResp.vErrorMsg = "Please Pass the Valid Employee ID";
+                }
+                else
+                {
+                    if (Con.State == ConnectionState.Broken)
+                    { Con.Close(); }
+                    if (Con.State == ConnectionState.Closed)
+                    { Con.Open(); }
+
+                    SqlCommand cmd = new SqlCommand("USP_Employee", Con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpId", objReq.EmpId);
+                    cmd.Parameters.AddWithValue("@QueryType", "FetchEmpDetails");
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    int i = 0;
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        objResp.EmpId = Convert.ToInt64(ds.Tables[0].Rows[i]["EmployeeId"]);
+                        objResp.EmpName = Convert.ToString(ds.Tables[0].Rows[i]["Name"]);
+                        objResp.EmpMobile = Convert.ToString(ds.Tables[0].Rows[i]["EmpMobile"]);
+                        objResp.Department = Convert.ToString(ds.Tables[0].Rows[i]["Department"]);
+                        objResp.Location = Convert.ToString(ds.Tables[0].Rows[i]["Location"]);
+
+                        objResp.vErrorMsg = "Success";
+                        objResp.vErrorCode = 200;
+                    }
+                    else
+                    {
+                        objResp.vErrorMsg = "Employee details are not found.";
+                        objResp.vErrorCode = 400;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                Logger.WriteLog("Function Name : Fn_Fetch_EmployeeDetails", " " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+                objResp.vErrorCode = 500;
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+        public clsEmployee Fn_Update_EmployeeDetails(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                SqlCommand cmd = new SqlCommand("USP_Employee", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", objReq.EmpId);                
+                cmd.Parameters.AddWithValue("@Name", objReq.EmpName);
+                cmd.Parameters.AddWithValue("@Department", objReq.Department);
+                cmd.Parameters.AddWithValue("@Salary", objReq.Salary);
+                cmd.Parameters.AddWithValue("@EmpPassword", objReq.EmpPassword);
+                cmd.Parameters.AddWithValue("@EmpMobile", objReq.EmpMobile);
+                cmd.Parameters.AddWithValue("@Location", objReq.Location);
+                cmd.Parameters.AddWithValue("@QueryType", "UpdateEmpDetails");
+
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    objResp.vErrorMsg = "Success";
+                    objResp.vErrorCode = 200;
+                }
+                else
+                {
+                    objResp.vErrorMsg = "Employee updation is failed";
+                    objResp.vErrorCode = 400;
+                }
+            }
+            catch (Exception exp)
+            {
+                Logger.WriteLog("Function Name : Fn_Update_EmployeeDetails", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+                objResp.vErrorCode = 500;
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+        public clsEmployee Fn_Delete_Employee(clsEmployee objReq)
+        {
+            var objResp = new clsEmployee();
+            try
+            {
+                if (Con.State == ConnectionState.Broken)
+                { Con.Close(); }
+                if (Con.State == ConnectionState.Closed)
+                { Con.Open(); }
+
+                SqlCommand cmd = new SqlCommand("USP_Employee", Con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@EmpId", objReq.EmpId);
+                cmd.Parameters.AddWithValue("@QueryType", "DeleteEmployee");
+
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    objResp.vErrorMsg = "Success";
+                    objResp.vErrorCode = 200;
+                }
+                else
+                {
+                    objResp.vErrorMsg = "Employee Deletion failed";
+                    objResp.vErrorCode = 400;
+                }
+            }
+            catch (Exception exp)
+            {
+                Logger.WriteLog("Function Name : Fn_Delete_Employee", " " + "Error Msg : " + exp.Message.ToString(), new StackTrace(exp, true));
+                objResp.vErrorMsg = exp.Message.ToString();
+                objResp.vErrorCode = 500;
+            }
+            finally
+            {
+                Con.Close();
+            }
+            return objResp;
+        }
+
+
+
+    }
+}
