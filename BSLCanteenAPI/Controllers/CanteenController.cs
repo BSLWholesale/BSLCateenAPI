34d354f0cf16b38e8eb1b412ddd70e5d1498bfbd
Kanchan <banswaraws@gmail.com>
2026-06-30 18:39:33 +0530

    Added the new Controller function i.e. Fn_Emp_CouponGeneration() and Fn_Fetch_EmpCouponId() in CanteenController.cs - Kanchan

--- BSLCanteenAPI/Controllers/CanteenController.cs
+++ BSLCanteenAPI/Controllers/CanteenController.cs
@@ -2,16 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using BSLCanteenAPI.DAL;
+using BSLCanteenAPI.Models;
 
 namespace BSLCanteenAPI.Controllers
 {
-    public class CanteenController : Controller
+    public class CanteenController : ApiController
     {
         // GET: Canteen
-        public ActionResult Index()
+
+        DALCanteen _DALCanteen = new DALCanteen();
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/Canteen/Fn_Emp_CouponGeneration")]
+        public clsCouponOrder Fn_Emp_CouponGeneration(clsCouponOrder objReq)
         {
-            return View();
+            var objResp = new clsCouponOrder();
+            objResp = _DALCanteen.Fn_Emp_CouponGeneration(objReq);
+            return objResp;
         }
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/Canteen/Fn_Fetch_EmpCouponId")]
+        public List<clsCouponOrder> Fn_Fetch_EmpCouponId(clsCouponOrder objReq)
+        {
+            var objResp = new List<clsCouponOrder>();
+            objResp = _DALCanteen.Fn_Fetch_EmpCouponId(objReq);
+            return objResp;
+        }
+
+
     }
 }
