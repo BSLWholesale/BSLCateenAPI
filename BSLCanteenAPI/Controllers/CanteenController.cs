cff2d680ec2316df8cbcc39a9f74b268460b3ae0
Kanchan <banswaraws@gmail.com>
2026-06-30 10:10:29 +0530

    New Controller added i.e. CanteenController.cs - Kanchan

---
+++ BSLCanteenAPI/Controllers/CanteenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BSLCanteenAPI.Controllers
+{
+    public class CanteenController : Controller
+    {
+        // GET: Canteen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
