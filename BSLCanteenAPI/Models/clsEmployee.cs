81dfc2fa8a0870a190c8bb2b6439149974cdc19e
Kanchan <banswaraws@gmail.com>
2026-07-02 11:59:08 +0530

    Modified in clsEmployee.cs rename the object in clsEmployee modal class - Kanchan

--- BSLCanteenAPI/Models/clsEmployee.cs
+++ BSLCanteenAPI/Models/clsEmployee.cs
@@ -13,7 +13,7 @@ namespace BSLCanteenAPI.Models
         public string Salary { get; set; }
         public string EmpPassword { get; set; }
         public string EmpMobile { get; set; }
-        public string Location { get; set; }
+        public string EmpLocation { get; set; }
         public string EmpRole { get; set; }
         public int CreatedBy { get; set; }
         public string CreatedOn { get; set; }
